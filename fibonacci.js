15b7d31eb371f09bd58e723f400c736eb0fecb58
Renee <rchar20@gmail.com>
2016-09-22 12:28:49 -1000

    Implements memoization

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,28 @@
-module.exports = function fib(n) {
+let cache = {};
 
-	  if (n===2) { 
-	  	return [1,1];
-	  }
-	  if (n===1) {
-	  	return [1];
-	  }
-	  let a = fib(n-1);
-	  a[a.length] = fib(n-1)[fib(n-1).length -1] + fib(n-1)[fib(n-1).length -2];
-	  return a;
+function mfib(n) {
+	if(!cache.hasOwnProperty(n)){
+		cache[n]= fib(n);
+	}
+	return cache[n];
 }
 
+
+function fib(n) {
+
+	  if (n===2) { 
+	  	let arr = mfib(1)
+	  	arr.push(1);
+	  	return arr;
+	  }
+	  if (n<=1) {
+	  	return [1];
+	  }
+
+	  let resultArr = mfib(n-1);
+	  resultArr.push(mfib(n-1)[mfib(n-1).length -1] + mfib(n-1)[mfib(n-1).length - 2]);
+	  return resultArr;
+}
+
+
+console.log('fib(9)',fib(9));
--- test/fibonacci.spec.js
+++ test/fibonacci.spec.js
@@ -1,13 +1,13 @@
 const Chai = require('chai');
 const expect = Chai.expect;
 const fibonacci = require('../fibonacci');
-
+let emptyArr = [];
 
 describe('fibonacci', () => {
 	it('should be a function', () => {
 		expect(fibonacci).to.be.a('function');
 	});
 	it('should return an array of fibonacci numbers. Amount of fibonacci numbers listed should be the number passed in', () => {
-		expect(fibonacci(5)).to.deep.equal([1,1,2,3,5]);
+		expect(fibonacci(5,emptyArr)).to.deep.equal([1,1,2,3,5]);
 	});
 });
